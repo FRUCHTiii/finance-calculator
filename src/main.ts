ed68f9526865b91bbc2eb0ef56cbb801094b86c8
FRUCHTiii <57792137+FRUCHTiii@users.noreply.github.com>
2025-01-01 11:04:35 +0100

    set host to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = process.env.PORT ?? 3000;
+  const host = process.env.HOST ?? '0.0.0.0';
+  
+  await app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+  });
 }
 bootstrap();
